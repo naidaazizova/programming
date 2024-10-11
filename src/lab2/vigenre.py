157b688abf1cb7ae3c5a6bcc23bfe281970bfed9
naidaazizova <naida.azi@yandex.ru>
2024-10-11 19:26:54 +0300

    [feat]: added addition function for encrypt_vigenre() and decrypt_vigenre()

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,9 +9,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_length = len(keyword)
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            shift = ord(keyword[i % keyword_length]) - ord('A')
+            if plaintext[i].islower():
+                encrypted_char = chr((ord(plaintext[i]) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_char = chr((ord(plaintext[i]) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += encrypted_char
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
-
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
@@ -24,5 +34,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_length = len(keyword)
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            shift = ord(keyword[i % keyword_length]) - ord('A')
+            if ciphertext[i].islower():
+                decrypted_char = chr((ord(ciphertext[i]) - ord('a') - shift + 26) % 26 + ord('a'))
+            else:
+                decrypted_char = chr((ord(ciphertext[i]) - ord('A') - shift - 26) % 26 + ord('A'))
+            plaintext += decrypted_char
+        else:
+            plaintext += ciphertext[i]
     return plaintext
