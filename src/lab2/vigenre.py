45ff11dffe5179c7736ba83745de5adf431e8b01
naidaazizova <naida.azi@yandex.ru>
2024-10-12 15:15:49 +0300

    [feat]: added addition function for is_prime(), gcd(), multiplicative_inverse(), generate_keypair() and encrypt, decrypt

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -5,22 +5,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
     'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
+    >>> encrypt_vigenere("ATT ACKATDAWN", "LEMON")
+    'LXF OPVEFRNHR'
     """
     ciphertext = ""
     keyword = keyword.upper()
     keyword_length = len(keyword)
+    keyword_space = 0
     for i in range(len(plaintext)):
         if plaintext[i].isalpha():
-            shift = ord(keyword[i % keyword_length]) - ord('A')
+            shift = ord(keyword[(i - keyword_space)  % keyword_length]) - ord('A')
             if plaintext[i].islower():
                 encrypted_char = chr((ord(plaintext[i]) - ord('a') + shift) % 26 + ord('a'))
             else:
                 encrypted_char = chr((ord(plaintext[i]) - ord('A') + shift) % 26 + ord('A'))
+
             ciphertext += encrypted_char
         else:
             ciphertext += plaintext[i]
+            keyword_space += 1
     return ciphertext
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -30,15 +33,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
     'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
+    >>> decrypt_vigenere("LXF OPVEFRNHR", "LEMON")
+    'ATT ACKATDAWN'
     """
     plaintext = ""
     keyword = keyword.upper()
     keyword_length = len(keyword)
+    keyword_space = 0
     for i in range(len(ciphertext)):
         if ciphertext[i].isalpha():
-            shift = ord(keyword[i % keyword_length]) - ord('A')
+            shift = ord(keyword[(i - keyword_space)  % keyword_length]) - ord('A')
             if ciphertext[i].islower():
                 decrypted_char = chr((ord(ciphertext[i]) - ord('a') - shift + 26) % 26 + ord('a'))
             else:
@@ -46,4 +50,5 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             plaintext += decrypted_char
         else:
             plaintext += ciphertext[i]
+            keyword_space += 1
     return plaintext
